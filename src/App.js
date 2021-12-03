8d3ecb81bec809001a0776d2d027ecdf1fb780f7
Nikola Djukic <nikola.djukic@amplitudo.me>
2021-12-03 20:08:32 +0100

    add header with localization dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {memo} from "react";
 import eng from "./language/eng.json";
 import mne from "./language/mne.json";
+import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import {
 	setTranslations,
 	setDefaultLanguage,
@@ -13,17 +15,37 @@ setTranslations({eng, mne});
 setDefaultLanguage("eng");
 
 function App() {
-	const handleSetLanguage = (key) => () => {
-		getLanguage() === "eng" ? setLanguage("mne") : setLanguage("eng");
+	const handleSetLanguage = (lang) => () => {
+		setLanguage(lang);
 	};
 
 	return (
 		<div className="App">
-			<h1>{t("home.title")}</h1>
-
-			<button type="button" onClick={handleSetLanguage("mne")}>
-				Switch language
-			</button>
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">
+						<h1>{t("home.title")}</h1>
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse
+						id="basic-navbar-nav"
+						className="justify-content-end">
+						<Nav className="me-auto">
+							<NavDropdown
+								title={t("language.changeLanguage")}
+								id="basic-nav-dropdown">
+								<NavDropdown.Item onClick={handleSetLanguage("eng")}>
+									{t("language.en")}
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item onClick={handleSetLanguage("mne")}>
+									{t("language.me")}
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		</div>
 	);
 }
